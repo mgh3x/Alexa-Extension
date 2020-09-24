73045c60ba0f718b35b9918532a86f472a5dccb0
cyberspace-power <gjs3qd@virginia.edu>
2020-09-24 01:50:44 -0400

    tried adding functionality to wait for DOM

--- scrape.js
+++ scrape.js
@@ -1,6 +1,15 @@
-let audio= document.getElementsByClassName("customer-transcript");
-if(audio.length == 0)
-    chrome.runtime.sendMessage("you suck");
-for(let i = 0; i < audio.length; i++) {
-    chrome.runtime.sendMessage(audio[i].innerText);
+if(document.readyState === 'loading') {
+    chrome.runtime.addEventListener("DOMContentLoaded", fire)
 }
+else {
+    fire();
+}
+
+function fire() {
+    let audio= document.getElementsByClassName("customer-transcript");
+        if(audio.length == 0)
+            chrome.runtime.sendMessage("you suck");
+        for(let i = 0; i < audio.length; i++) {
+            chrome.runtime.sendMessage(audio[i].innerText);
+        }
+    };
