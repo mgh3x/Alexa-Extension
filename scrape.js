cdd753389da2860bc25fe75f31981f7baaa0e3be
cyberspace-power <gjs3qd@virginia.edu>
2020-09-23 21:34:41 -0400

    Changed scrape

--- scrape.js
+++ scrape.js
@@ -1,1 +1,1 @@
-chrome.runtime.sendMessage(document.getElementsByClassName("record-item-text customer transcript")[0].innerText);
+chrome.runtime.sendMessage(document.getElementsByClassName("record-item-text customer transcript"));
