42e589fe56835e904e4fe7b149c510c68643a912
cyberspace-power <gjs3qd@virginia.edu>
2020-09-24 01:23:27 -0400

    small updates

--- scrape.js
+++ scrape.js
@@ -1,4 +1,6 @@
 let audio= document.getElementsByClassName("customer-transcript");
+if(audio.length == 0)
+    chrome.runtime.sendMessage("you suck");
 for(let i = 0; i < audio.length; i++) {
     chrome.runtime.sendMessage(audio[i].innerText);
 }
