4bf6f77fdddf1042e13e49a5dd17dce0711e1905
cyberspace-power <gjs3qd@virginia.edu>
2020-10-03 14:14:24 -0400

    added fetch to scrape.js

--- scrape.js
+++ scrape.js
@@ -1,4 +1,6 @@
-if(document.readyState === 'loading') {
+// Initial commented out code meant for https://www.amazon.com/alexa-privacy/apd/rvh
+
+/*if(document.readyState === 'loading') {
     chrome.runtime.addEventListener("DOMContentLoaded", printThing)
 }
 else {
@@ -16,4 +18,19 @@ function fire() {
 
 function printThing() {
     chrome.runtime.sendMessage("Nothing to report");
+}*/
+
+// Following code meant for https://alexa.amazon.com/spa/index.html#cards
+async function getAlexaDom() {
+    let response = await fetch("https://alexa.amazon.com/spa/index.html#cards");
+
+    if (response.ok) { // if HTTP-status is 200-299
+    // get the response body (the method explained below)
+    let json = await response.text();
+    console.log(json);
+    } 
+    else
+        alert("HTTP-Error: " + response.status);
 }
+
+getAlexaDom();
